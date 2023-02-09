8bdd7048c02d8d7b5a1a573ea941ba564e7d536b
Michael2150 <michaelgerb2150@gmail.com>
2023-02-09 21:08:47 +0000

    gitignore change

---
+++ personal_website/src/firebase/Firebase.ts
@@ -0,0 +1,11 @@
+// Set up Firebase in typescript
+// https://www.youtube.com/watch?v=9kRgVxULbag
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+const config = {
+
+}
